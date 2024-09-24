80b06749ffd24c01ea853a98d0a8d4b0b896cad2
Sho Kaneko <kanekoshoyu@gmail.com>
2024-09-24 14:42:36 +0100

    simplify

--- template/index.js
+++ template/index.js
@@ -2,24 +2,32 @@ import { File } from '@asyncapi/generator-react-sdk';
 // import { GetSubscriberFlags, GetPublisherFlags, hasPubOrSub, hasSub } from '../components/common';
 // import { publishConfigsFrom } from '../components/Handlers';
 
-function renderDependencies(moduleName, doc) {
-  let imports = `
-import (
-  "context"
-  "log"
-  "os"
-  "os/signal"
-  "syscall"
-  "${moduleName}/asyncapi"
-)
-  `;
-  return imports;
+// guidelines
+// render_X() -> returns <file>
+// conent_X() -> returns String
+
+function render_schema({ asyncapi }) {
+  const schemas = asyncapi.allSchemas();
+  const files = [];
+  // schemas is an instance of the Map
+  schemas.forEach((schema) => {
+
+    files.push(
+      // We return a react file component and each time we do it, the name of the generated file will be a schema name
+      // Content of the file will be a variable representing schema
+      <File name={`${schema.id()}.js`}>
+        const {schema.id()} = {JSON.stringify(schema._json, null, 2)}
+      </File>
+    );
+  });
+  return files;
 }
 
-function main_rs_content() {
+// 
+function content_main_rs() {
   return `
 // main.rs
-${renderDependencies(params.moduleName, asyncapi)}
+use tokio;
 
 fn main() {
   println!("Hello world");
@@ -27,20 +35,18 @@ fn main() {
 `;
 }
 
+function render_main_rs() {
+  return [
+    <File name="main.rs">{content_main_rs()}</File>
+  ];
+}
+
 // return array of file objects for render 
 export default function ({ asyncapi, params }) {
-  // accept inputs
+  // accept inputsm asynca
   // parse the document into some sort of AsyncAPI DSL
   // generate the Rust Code
   console.log("Hello, World!");
 
-  const informativeErrMsg = `
-    Since there are no supported channels in the asyncapi document there is no code to output
-    Currently supported channels are:
-      AMQP Subscriber
-      AMQP Publisher
-  `;
-  return [
-    <File name="main.rs">{main_rs_content()}</File>
-  ];
+  return render_main_rs() + render_schema(asyncapi);
 }
